b2cf0cbaca368ba57a4e54e51df6186630e355bf
Aemal <aemal7@icloud.com>
2024-07-22 05:09:42 +0500

    small improvemets and comments added

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const form = document.querySelector('#item-form');
+const ul = document.querySelector('.items');
 
 function addItem(e) {
   e.preventDefault();
   const formData = new FormData(form);
   const content = formData.get('item');
 
+  // Validate input: empty string can not be added
   if (!content) {
+    alert('No item entered!');
     return;
   }
 
-  const ul = document.querySelector('.items');
   ul.appendChild(makeListItem(content));
+  form.reset();
+  console.log('Item added succesfully!');
 }
 
+// Function to create list item to be added
 function makeListItem(content) {
   li = document.createElement('li');
   li.appendChild(document.createTextNode(content));
 
-  li.appendChild(makeButton('remove-item btn-link text-red'));
+  li.appendChild(makeDeleteButton('remove-item btn-link text-red'));
   return li;
 }
 
-function makeButton(classes) {
+// Function to create delete item button
+function makeDeleteButton(classes) {
   button = document.createElement('button');
   button.className = classes;
 
@@ -29,6 +35,7 @@ function makeButton(classes) {
   return button;
 }
 
+// Function to create icon of delete button
 function makeIcon(classes) {
   icon = document.createElement('i');
   icon.className = classes;
